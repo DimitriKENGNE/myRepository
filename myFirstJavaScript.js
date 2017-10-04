b1d02dee160fb472cedfa4579724c7eb77dfa25e
DimitriKENGNE <m.dimitri.kengne@gmail.com>
2017-10-04 16:51:00 +0100

    Suppression du code JavaScript relatif à la barre de navigation; on utilise plutôt du CSS pour le positionnement. ET Avec un code JavaScript, les mot entrées dans les formulaires sont automatiquement mis en majuscule

--- myFirstJavaScript.js
+++ myFirstJavaScript.js
@@ -7,26 +7,48 @@ function hideDate() {
 }
 
 
-function navigbar(x) {
-	if (document.body.scrollTop > 225){
-		x.style.position = "fixed";
-		x.style.top = "0";
-	}
+/*  			function navigbar(x) {
+					if (document.body.scrollTop > 225){
+						x.style.position = "fixed";
+						x.style.top = "0";
+					}
 	
-}
+				}
 
-window.onscroll = function() {navigbar(bar)};	
+				window.onscroll = function() {navigbar(bar)};	   */
 	
 function afficher(x){
 	var x= document.getElementById("box");
 	x.style.display="block";
 }
 
-/*  function cacher(x){
-		var x= document.getElementById("box");
-		x.style.display="none";
-	}
+/*  			function cacher(x){
+					var x= document.getElementById("box");
+					x.style.display="none";
+				}
 
-	document.getElementById("box").onmouseleave = cacher();     */
+				document.getElementById("box").onmouseleave = cacher();     */
 	
-	
+
+
+
+
+function majusCule1() {
+    var x = document.getElementById("fname1");
+    x.value = x.value.toUpperCase();
+}
+
+function majusCule2() {
+    var x = document.getElementById("fname2");
+    x.value = x.value.toUpperCase();
+}
+
+function majusCule3() {
+    var x = document.getElementById("fname3");
+    x.value = x.value.toUpperCase();
+}
+
+function majusCule4() {
+    var x = document.getElementById("fname4");
+    x.value = x.value.toUpperCase();
+}
