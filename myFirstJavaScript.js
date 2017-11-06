743ece67008ab50039c28feb8a942e136ab61f96
DimitriKENGNE <m.dimitri.kengne@gmail.com>
2017-11-06 10:04:18 +0100

    Modifications Oubliées

--- myFirstJavaScript.js
+++ myFirstJavaScript.js
@@ -1,5 +1,76 @@
 function displayDate() {
-    document.getElementById("time").innerHTML = Date();
+	var d = new Date();
+	var day = "Sunday";
+	switch (d.getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+	var month = "January";
+	switch (d.getMonth()) {
+    case 0:
+        month = "January";
+        break;
+	case 1:
+        month = "Febuary";
+        break;
+    case 2:
+        month = "March";
+        break;
+	case 3:
+        month = "April";
+        break;
+	case 4:
+        month = "May";
+        break;
+	case 5:
+        month = "June";
+        break;
+	case 6:
+        month = "July";
+        break;
+	case 7:
+        month = "August";
+        break;
+	case 8:
+        month = "September";
+        break;
+	case 9:
+        month = "October";
+        break;
+	case 10:
+        month = "November";
+        break;
+	case 11:
+        month = "December";
+        break;
+	}
+	
+    document.getElementById("time").innerHTML = day  + d.getDate() + month + d.getFullYear()
+	+ "<br> <br>" + d.getHours() + "H" + " " + d.getMinutes() + "min" + " " + d.getSeconds() + "sec";
+	
+	/*
+	
+	document.getElementById("time").style.color = "red";
+	document.getElementById("time").style.text-align = "center";
+	document.getElementById("time").style.font-size = "200%";            */
 }
 
 function hideDate() {
@@ -35,7 +106,10 @@ function afficher(x){
 
 function majusCule1() {
     var x = document.getElementById("fname1");
+	console.log(x);
     x.value = x.value.toUpperCase();
+	console.log(x.value);
+	
 }
 
 function majusCule2() {
@@ -50,5 +124,10 @@ function majusCule3() {
 
 function majusCule4() {
     var x = document.getElementById("fname4");
+    x.value = x.value.toUpperCase();
+}
+
+function majusCule5() {
+    var x = document.getElementById("fname5");
     x.value = x.value.toUpperCase();
 }
